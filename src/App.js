70bd4b0e4aeebdafadcf6874f7af43397c9389f1
RobotDream-lore <furcas.lorenzo@gmail.com>
2022-02-09 23:59:10 +0100

    First commmit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import useToken from './hooks/useToken';
+import Logout from './components/Logout';
+import React from 'react'
+import FetchingTable from './components/FetchingTable';
 
 function App() {
+  
+  const multimedia_columns = React.useMemo(
+    () => [
+        {
+            Header: 'Title',
+            accessor: 'title',
+        },
+        {
+            Header: 'Preview',
+            accessor: 'preview',
+            className: "preview",
+        },
+        {
+            Header: 'Creation Date',
+            accessor: 'creation_date',
+        },
+        {
+            Header: 'Last Modify',
+            accessor: 'last_modify',
+        },
+        ],
+    []
+  )
+  const data = React.useMemo(() => { return [{title:"diocan", preview:"audio", creation_date:"aaa", last_modify:"aaaaa"}] },[] );
+
+  const {token, setToken, clearToken } = useToken();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      { token !== undefined ?
+        <div> 
+          <Logout clearToken={clearToken} /> 
+          <FetchingTable columns={multimedia_columns} url='http://127.0.0.1:8000/adamanager/multimedia'/>
+        </div> : 
+        <Login setToken={setToken} /> }
+    </div> 
   );
 }
 
